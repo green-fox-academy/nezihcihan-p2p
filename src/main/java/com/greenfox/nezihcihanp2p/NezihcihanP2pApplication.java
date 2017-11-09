0b2557d71f0f91f53d765768530dfc666bc4c63c
nezih <nezihcihan@hotmail.com>
2017-11-09 15:27:42 +0100

    My first commit

---
+++ src/main/java/com/greenfox/nezihcihanp2p/NezihcihanP2pApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.nezihcihanp2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezihcihanP2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NezihcihanP2pApplication.class, args);
+	}
+}
